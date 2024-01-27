78dff1893d800e373c08d645a0de93c136bef1fd
ArshadSherif <am5682@srmist.edu.in>
2024-01-27 21:02:55 +0530

    bug fix

--- client/src/app/posts/new-post/new-post.component.ts
+++ client/src/app/posts/new-post/new-post.component.ts
@@ -34,7 +34,7 @@ export class NewPostComponent implements OnInit {
       permalink: ['', Validators.required],
       excerpt: ['', [Validators.required, Validators.minLength(10)]],
       category: ['', Validators.required],
-      postImg: [null, Validators.required],
+      postImg: [File, Validators.required],
       content: ['', Validators.required],
     });
   }
@@ -83,9 +83,6 @@ export class NewPostComponent implements OnInit {
   onSubmit() {
 
     let splitted=this.postForm.value.category.split('-');
-
-
-
     console.log(this.postForm.value)
     const postData: Post = {
       title: this.postForm.value.title,
--- server/src/model/post.ts
+++ server/src/model/post.ts
@@ -3,18 +3,19 @@ import categoryModel from './categories';
 const postSchema = new mongoose.Schema({
     title : String,
     postImg : String,
-    permalink:String,
-    category:{
-        categoryId:{ type: mongoose.Types.ObjectId, ref: categoryModel },
-        category:{ type: mongoose.Types.ObjectId, ref: categoryModel }
-    },
-    // postImgPath:String,
-    excerpt:String,
-    content:String,
-    isFeatured:Boolean,
-    views:Number,
-    status:String,
-    createdAt:Date
+    // permalink:String,
+    
+    // category:{
+    //     categoryId:{ type: mongoose.Types.ObjectId, ref: categoryModel },
+    //     category:{ type: mongoose.Types.ObjectId, ref: categoryModel }
+    // },
+    // // postImgPath:String,
+    // excerpt:String,
+    // content:String,
+    // isFeatured:Boolean,
+    // views:Number,
+    // status:String,
+    // createdAt:Date
 
 });
 
